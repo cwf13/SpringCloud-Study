d463df2576825d76e814216ad7120eb39d6900ff
cwf13 <952153979@qq.com>
2019-12-11 16:29:46 +0800

    45354

---
+++ springcloud-config-eureka/src/test/java/com/example/springcloudconfigeureka/SpringcloudConfigEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudconfigeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudConfigEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
