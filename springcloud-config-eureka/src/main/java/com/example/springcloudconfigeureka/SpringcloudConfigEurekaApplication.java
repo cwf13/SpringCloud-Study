7aa19352f4f28929d9027679e8c70e8d5de7c890
cwf13 <952153979@qq.com>
2019-12-11 17:17:56 +0800

    66

--- springcloud-config-eureka/src/main/java/com/example/springcloudconfigeureka/SpringcloudConfigEurekaApplication.java
+++ springcloud-config-eureka/src/main/java/com/example/springcloudconfigeureka/SpringcloudConfigEurekaApplication.java
@@ -2,9 +2,10 @@ package com.example.springcloudconfigeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class SpringcloudConfigEurekaApplication {
 
     public static void main(String[] args) {
